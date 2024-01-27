924c9754086d91cd11b3a9a732acdd580939a751
NoeH17 <ibarrazurdo@hotmail.com>
2024-01-27 00:07:04 -0600

    Avances de manejo de errores

--- html/procesarFormulario.php
+++ html/procesarFormulario.php
@@ -3,26 +3,40 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+ini_set('log_errors', 1);
+ini_set('error_log', 'C:/xampp/htdocs/BAN---Jardineria-y-Mantenimiento-Industrial/Errores/error_log.txt');
+
 require '../vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $recaptcha_secret = '6LfOmVopAAAAAJsMC2QuAf6Krv_VMW3gG33x242R';
+    $recaptcha_secret = '6LfOmVopAAAAAJsMC2QuAf6Krv_VMW3gG33x242R';    
     $recaptcha_response = $_POST['g-recaptcha-response'];
 
     $recaptcha_url = "https://www.google.com/recaptcha/api/siteverify?secret={$recaptcha_secret}&response={$recaptcha_response}";
     $recaptcha_data = json_decode(file_get_contents($recaptcha_url));
 
     if (!$recaptcha_data->success) {
-        die('Captcha no válido');
+        echo '<script>
+            alert("Captcha no válido");
+                window.onload = function() {
+                    setTimeout(function() {
+                    window.location.href = "Contacto.html";
+                }, 0);
+            };
+        </script>';
+        
+        $captcha_error_info = json_encode($recaptcha_data);
+        error_log("Error en el captcha: {$error_message}. Detalles: {$captcha_error_info}", 0);
+        die($error_message);
     }
 
-    $nombre = $_POST['nombre'];
-    $correo = $_POST['correo'];
-    $fuente = $_POST['fuente'];
-    $mensaje = $_POST['mensaje'];
-    $empresa = $_POST['empresa'];
-    $telefono = $_POST['telefono'];
-    $servicio = $_POST['servicio'];
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $correo = htmlspecialchars($_POST['correo']);
+    $fuente = htmlspecialchars($_POST['fuente']);
+    $mensaje = htmlspecialchars($_POST['mensaje']);
+    $empresa = htmlspecialchars($_POST['empresa']);
+    $telefono = htmlspecialchars($_POST['telefono']);
+    $servicio = htmlspecialchars($_POST['servicio']);
 
     $mail = new PHPMailer(true);
 
@@ -32,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'devzone.playeras@gmail.com'; 
-        $mail->Password   = 'yxbe fhqt wklu mtkq ';
+        $mail->Password   = 'yxbe fhqt wklu mtkq';       
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port       = 465;
 
@@ -50,12 +64,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         ";
 
         $mail->send();
-        echo '<script>alert("Mensaje enviado correctamente");</script>';
-        echo '<script>setTimeout(function(){ window.location.href = "Contacto.html"; }, 10);</script>';
-        // header('Location: Contacto.html');
+
+        echo '<script>alert("Tu mensaje ha sido enviado correctamente. Gracias por ponerte en contacto.");
+                window.onload = function() {
+                setTimeout(function() {
+                window.location.href = "Contacto.html";
+                }, 0);
+            };
+        </script>';
+
         exit();
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+        echo '<script>
+            alert("Hubo un problema al procesar tu solicitud. Por favor, contacta al soporte técnico en support@example.com para obtener ayuda.");
+                window.onload = function() {
+                    setTimeout(function() {
+                    window.location.href = "Contacto.html";
+                }, 0);
+            };
+        </script>';
+        $smtp_error_info = $mail->ErrorInfo;
+
+    error_log("Error al enviar el correo: {$smtp_error_info}", 0);
+        die($error_message);
     }
 } else {
     echo "Invalid request method";
